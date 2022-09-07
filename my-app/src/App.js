0558656043e7d58e0ecc803f5dea16db4f2663c6
Jun Hwang <jhwang2525@gmail.com>
2022-09-06 23:24:20 -0700

    header and footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/header"
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BsHouseDoorFill } from "react-icons/bs";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>
+    <Link to="/"><BsHouseDoorFill /></Link>
+    <Link to="/">Home</Link>
+    </Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Home />
     </div>
   );
 }
---
+++ my-app/src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+    return <h1>LineHop</h1>;
+};
+
+export default Home;
+
+
